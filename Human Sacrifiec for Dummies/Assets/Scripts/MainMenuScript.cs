cfbdc49cf2d45dad548d7de931825c1c5c411515
Samsteve123 <Samstevenson321@gmail.com>
2019-10-06 11:17:49 -0700

    Main Menu
    main menu exists and switches scenes on button press

---
+++ Human Sacrifiec for Dummies/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button Play;
+    public Button Exit;
+
+    void Start()
+    {
+        Button exit = Exit.GetComponent<Button>();
+        Button play = Play.GetComponent<Button>();
+        play.onClick.AddListener(PlayOnClick);
+        exit.onClick.AddListener(ExitOnClick);
+    }
+   
+    void PlayOnClick()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    void ExitOnClick()
+    {
+        Application.Quit();
+    }
+}
