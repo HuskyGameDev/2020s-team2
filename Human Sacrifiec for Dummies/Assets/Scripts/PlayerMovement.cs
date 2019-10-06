03cc3c6a84d19bb24dfb85fe9255356c2988f50b
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-10-06 14:15:28 -0400

    added a* project and stevens scene

--- Human Sacrifiec for Dummies/Assets/Scripts/PlayerMovement.cs
+++ Human Sacrifiec for Dummies/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Vector2 targetPosition;
+    public Vector2 startPosition;
 
     private void Start()
     {
 
-        targetPosition.x = Mathf.Floor(gameObject.transform.position.x) + 0.5f;
+        targetPosition.x = Mathf.Round(gameObject.transform.position.x);
         targetPosition.y = Mathf.Floor(gameObject.transform.position.y) + 0.5f;
+        startPosition = targetPosition;
+
     }
 
     // Update is called once per frame
@@ -20,8 +23,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition.x = Mathf.Floor(targetPosition.x) + 0.5f;
+            targetPosition.x = Mathf.Round(targetPosition.x);
             targetPosition.y = Mathf.Floor(targetPosition.y) + 0.5f;
+        }
+        float magnitude = Vector2.Distance(startPosition, targetPosition);
+        if (magnitude <= 3)
+        {
+
+        }
+        else
+        {
+            targetPosition = startPosition;
         }
 
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
