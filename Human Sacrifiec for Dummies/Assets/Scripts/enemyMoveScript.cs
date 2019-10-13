ae352560ddd3142d713164644d117097a45b63dd
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-10-13 14:51:40 -0400

    turn system added

---
+++ Human Sacrifiec for Dummies/Assets/Scripts/enemyMoveScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMoveScript : MonoBehaviour
+{
+    // turn system stuff
+    public turnSystemScript TurnSystem;
+    public TurnClass turnClass;
+    public bool isTurn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // turn system stuff
+        TurnSystem = GameObject.Find("Turn-basedSystem").GetComponent<turnSystemScript>();
+
+        foreach (TurnClass tc in TurnSystem.playersGroup)
+        {
+            if (tc.playerGameObject.name == gameObject.name)
+            {
+                turnClass = tc;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //turn system stuff
+        isTurn = turnClass.isTurn;
+
+        // check if its your turn
+        if(isTurn)
+        {
+            // if your turn go to wait and move
+            StartCoroutine("WaitAndMove");
+        }
+    }
+
+    IEnumerator WaitAndMove()
+    {
+        // wait for 1 second
+        yield return new WaitForSeconds(1f);
+
+        // movement goes here
+
+        // end turn
+        isTurn = false;
+        turnClass.isTurn = isTurn;
+        turnClass.wasTurnPrev = true;
+
+        StopCoroutine("WaitAndMove");
+    }
+}
---
+++ Human Sacrifiec for Dummies/Assets/Scripts/turnSystemScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnSystemScript : MonoBehaviour
+{
+    public List<TurnClass> playersGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTurns();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTurns();
+    }
+
+    void ResetTurns()
+    {
+        for (int i = 0; i < playersGroup.Count; i++)
+        {
+            if (i ==0)
+            {
+                playersGroup[i].isTurn = true;
+                playersGroup[i].wasTurnPrev = false;
+            }
+            else
+            {
+                playersGroup[i].isTurn = false;
+                playersGroup[i].wasTurnPrev = false;
+            }
+        }
+    }
+
+    void UpdateTurns()
+    {
+        for(int i = 0;i<playersGroup.Count;i++)
+        {
+            if (!playersGroup[i].wasTurnPrev)
+            {
+                playersGroup[i].isTurn = true;
+                break;
+            }
+            else if (i == playersGroup.Count -1 && playersGroup[i].wasTurnPrev)
+            {
+                ResetTurns();
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class TurnClass
+{
+    public GameObject playerGameObject;
+    public bool isTurn = false;
+    public bool wasTurnPrev = false;
+
+
+}
