9d5314c07da63edded8d34346ebbb5be464809ca
Samsteve123 <Samstevenson321@gmail.com>
2019-10-13 15:34:35 -0400

    Ability Choosing System and UI
    Script and UI elements needed for choosing 3 abilities created and functioning.

--- Human Sacrifiec for Dummies/Assets/Scripts/SelectAbility.cs
+++ Human Sacrifiec for Dummies/Assets/Scripts/SelectAbility.cs
@@ -5,19 +5,228 @@ using UnityEngine.UI;
 
 public class SelectAbility : MonoBehaviour
 {
-
+    //List of all available abilities.
+    string[] abilities = {"Staff Whack", "Fiendish Wisp", "Vicious Slap", 
+        "Forgotten Curse", "Infernal Bubble", "Shady Switcheroo", "Shadow Step" }; 
+    //This string store all of the names of the abilities the user has chosen.
+    string[] chosen = new string[3];
+    //Initialize all of the buttons in the scene.
     public Button Ability1;
+    public Button Ability2;
+    public Button Ability3;
+    public Button Ability4;
+    public Button Ability5;
+    public Button Ability6;
+    public Button Ability7;
+    public Button Reset;
+    public Button Confirm;
+
+    //Initialize a list for button objects needed to reset them when reset button is pressed.
+    public List<Button> buttons;
 
     private void Start()
     {
-        
+        //populate button list with the buttons
+        Button[] btns = this.GetComponentsInChildren<Button>(true);
+        for(int i = 0; i < btns.Length; i++)
+        {
+            buttons.Add(btns[i]);
+        }
+
+        //get all button components
+        Button ability1 = Ability1.GetComponent<Button>();
+        Button ability2 = Ability2.GetComponent<Button>();
+        Button ability3 = Ability3.GetComponent<Button>();
+        Button ability4 = Ability4.GetComponent<Button>();
+        Button ability5 = Ability5.GetComponent<Button>();
+        Button ability6 = Ability6.GetComponent<Button>();
+        Button ability7 = Ability7.GetComponent<Button>();
+        Button reset = Reset.GetComponent<Button>();
+        Button confirm = Confirm.GetComponent<Button>();
+
+        //add appropriate listeners to the buttons
+        ability1.onClick.AddListener(AddOneToBar);
+        ability2.onClick.AddListener(AddTwoToBar);
+        ability3.onClick.AddListener(AddThreeToBar);
+        ability4.onClick.AddListener(AddFourToBar);
+        ability5.onClick.AddListener(AddFiveToBar);
+        ability6.onClick.AddListener(AddSixToBar);
+        ability7.onClick.AddListener(AddSevenToBar);
+        reset.onClick.AddListener(resetButton);
+        confirm.onClick.AddListener(confirmButton);
     }
 
-    private void OnMouseDown()
+    //Check if the chosen ability array is filled at the given index
+    bool IsOpen(int index)
     {
-        if (Input.GetMouseButtonDown(0))
+        return (chosen[index] == null);
+    }
+
+    //reset all butons to be interactable again and remove choices from chosen array
+    void resetButton()
+    {
+        for(int i = 0; i < chosen.Length; i++)
         {
-            Debug.Log("click");
+            chosen[i] = null;
         }
+        for(int i = 0; i < buttons.Count ; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+
+    //confirm choices and advance to the next scene
+    void confirmButton()
+    {
+
+    }
+
+    //Add the first ability to whichever slot is available in the chosen array
+    void AddOneToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[0];
+        }else if (IsOpen(1))
+        {
+            chosen[1] = abilities[0];
+        }else if (IsOpen(2))
+        {
+            chosen[2] = abilities[0];
+        }
+        Ability1.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+
+
+    }
+
+    //Add the second ability to whichever slot is available in the chosen array
+    void AddTwoToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[1];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[1];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[1];
+        }
+        Ability2.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+
+    }
+
+    //Add the third ability to whichever slot is available in the chosen array
+    void AddThreeToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[2];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[2];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[2];
+        }
+        Ability3.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+    }
+
+    //Add the fourth ability to whichever slot is available in the chosen array
+    void AddFourToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[3];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[3];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[3];
+        }
+        Ability4.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+    }
+
+    //Add the fifth ability to whichever slot is available in the chosen array
+    void AddFiveToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[4];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[4];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[4];
+        }
+        Ability5.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+    }
+
+    //Add the sixth ability to whichever slot is available in the chosen array
+    void AddSixToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[5];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[5];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[5];
+        }
+        Ability6.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
+    }
+
+    //Add the seventh ability to whichever slot is available in the chosen array
+    void AddSevenToBar()
+    {
+        if (IsOpen(0))
+        {
+            chosen[0] = abilities[6];
+        }
+        else if (IsOpen(1))
+        {
+            chosen[1] = abilities[6];
+        }
+        else if (IsOpen(2))
+        {
+            chosen[2] = abilities[6];
+        }
+        Ability7.GetComponent<Button>().interactable = false;
+        Debug.Log(chosen[0]);
+        Debug.Log(chosen[1]);
+        Debug.Log(chosen[2]);
     }
 }
