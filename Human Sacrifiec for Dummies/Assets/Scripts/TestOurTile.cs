cd116a91985a1e83f366b42aac20651bc6df1a98
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-10-13 20:33:31 -0400

    Player can only move when clicking on the grid
    Grass is changed to not be below unpassable terrain.
    when clicking to move it checks if you click a tile on the grass tilemap, then moves you to that tile if true and ends your turn. (does not check for collision with enemies yet)
    added scripts to handle the grid checking

---
+++ Human Sacrifiec for Dummies/Assets/Scripts/TestOurTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TestOurTile : MonoBehaviour
+{
+    private WorldTile _tile;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var worldPoint = new Vector3Int((int) Mathf.Round(point.x), Mathf.FloorToInt(point.y), 0);
+
+            var tiles = GameTiles.instance.tiles; // This is our Dictionary of tiles
+
+            if (tiles.TryGetValue(worldPoint, out _tile))
+            {
+                print("Tile " + _tile.Name + " costs: " + _tile.Cost);
+                _tile.TilemapMember.SetTileFlags(_tile.LocalPlace, TileFlags.None);
+                _tile.TilemapMember.SetColor(_tile.LocalPlace, Color.gray);
+            }
+        }
+    }
+}
