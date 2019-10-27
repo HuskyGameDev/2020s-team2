76127c2cc24351fab7b5c8f16741642f7a6b5148
Samsteve123 <Samstevenson321@gmail.com>
2019-10-27 15:36:29 -0400

    HUD and start of in-game abilities
    Finished the HUD for as far as we are right now. Beginning to work on getting the abilities chosen in the selection screen into the in-game scene as well as coding the behavior for each ability.

---
+++ Human Sacrifiec for Dummies/Assets/Scripts/HUD.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text PlayerName;
+
+    public Button Ability1;
+    public Button Ability2;
+    public Button Ability3;
+    public Button Move;
+    public Button EndTurn;
+
+    public Text Turn;
+
+    public List<Button> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //populate button list with the buttons
+        Button[] btns = this.GetComponentsInChildren<Button>(true);
+        for (int i = 0; i < btns.Length; i++)
+        {
+            buttons.Add(btns[i]);
+        }
+
+        Button ability1 = Ability1.GetComponent<Button>();
+        Button ability2 = Ability2.GetComponent<Button>();
+        Button ability3 = Ability3.GetComponent<Button>();
+        Button move = Move.GetComponent<Button>();
+        Button endturn = EndTurn.GetComponent<Button>();
+        Text name = PlayerName.GetComponent<Text>();
+        Text turn = Turn.GetComponent<Text>();
+
+        ability1.onClick.AddListener(AbilityOneClick);
+        ability2.onClick.AddListener(AbilityTwoClick);
+        ability3.onClick.AddListener(AbilityThreeClick);
+        move.onClick.AddListener(MoveOnClick);
+        endturn.onClick.AddListener(EndOnClick);
+
+    }
+
+    void MoveOnClick()
+    {
+        Move.GetComponent<Button>().interactable = false;
+    }
+
+    void AbilityOneClick()
+    {
+        Ability1.GetComponent<Button>().interactable = false;
+    }
+
+    void AbilityTwoClick()
+    {
+        Ability2.GetComponent<Button>().interactable = false;
+    }
+
+    void AbilityThreeClick()
+    {
+        Ability3.GetComponent<Button>().interactable = false;
+    }
+
+    void EndOnClick()
+    {
+
+    }
+
+
+
+}
