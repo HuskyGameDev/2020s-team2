bff0ecdee633339933b688807882f52ba3c31798
sjbollno <43500323+sjbollno@users.noreply.github.com>
2019-10-29 18:58:21 -0400

    added sacrifice prefab and added trigger if sacrifice get to the alter

---
+++ Human Sacrifiec for Dummies/Assets/Scripts/Alter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // if the thing colliding with the alter has tag sacrifice
+        if (collision.CompareTag("Sacrifice"))
+        {
+            // you win code goes here.
+            print("You won by getting the Sacrifice to the alter");
+        }
+    }
+}
