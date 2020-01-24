9f0ac122a7505524dea6868037b24c0234c18ba1
Samsteve123 <Samstevenson321@gmail.com>
2020-01-24 17:45:07 -0500

    Fixed HUD bug
    Still need to change the yield wait to wait for a script to finish rather than wait for one second

--- Human Sacrifice for Dummies/Assets/Scripts/SelectAbility.cs
+++ Human Sacrifice for Dummies/Assets/Scripts/SelectAbility.cs
@@ -10,7 +10,7 @@ public class SelectAbility : MonoBehaviour
     string[] abilities = {"Staff Whack", "Fiendish Wisp", "Vicious Slap", 
         "Forgotten Curse", "Infernal Bubble", "Shady Switcheroo", "Shadow Step" }; 
     //This string store all of the names of the abilities the user has chosen.
-    public string[] chosen = new string[3];
+    string[] chosen = new string[3];
     //Initialize all of the buttons in the scene.
     public Button Ability1;
     public Button Ability2;
@@ -26,7 +26,7 @@ public class SelectAbility : MonoBehaviour
     public Text Chosen3;
 
     //Initialize a list for button objects needed to reset them when reset button is pressed.
-    public List<Button> buttons;
+    List<Button> buttons;
 
     private void Start()
     {
@@ -61,6 +61,8 @@ public class SelectAbility : MonoBehaviour
         ability7.onClick.AddListener(AddSevenToBar);
         reset.onClick.AddListener(resetButton);
         confirm.onClick.AddListener(confirmButton);
+
+        
     }
 
     //Check if the chosen ability array is filled at the given index
