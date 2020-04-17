03de6e3cdab68b7a01ddbe958b21a8eab37ae7dd
Samsteve123 <Samstevenson321@gmail.com>
2020-04-17 17:28:59 -0400

    Added ForgottenCurseAnimation.cs and ShadySwitcherooAnimation.cs

---
+++ Human Sacrifice for Dummies/Assets/Scripts/Animation Scripts/ShadySwitcherooAnimation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadySwitcherooAnimation : MonoBehaviour
+{
+    public void destroyAnimation()
+    {
+        Destroy(this.gameObject);
+    }
+}
